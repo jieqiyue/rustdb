e8f19d2391a28be6163a8be5c02c205ba7b685a7
roseduan <roseduan7@163.com>
2024-07-21 11:09:35 +0800

    13 从零实现 SQL 数据库—存储引擎基础方法

--- src/error.rs
+++ src/error.rs
@@ -34,3 +34,9 @@ impl From<Box<ErrorKind>> for Error {
         Error::Internal(value.to_string())
     }
 }
+
+impl From<std::io::Error> for Error {
+    fn from(value: std::io::Error) -> Self {
+        Error::Internal(value.to_string())
+    }
+}
